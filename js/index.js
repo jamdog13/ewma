3382ca05d4c2512725048ed5ccd4dc8f706ebc11
Palmer <Palmer's email>
2017-03-13 09:19:17 -0700

    close to ewmaing

--- js/index.js
+++ js/index.js
@@ -60,17 +60,27 @@ function accountCreate() {
 	// [START createwithemail]
 	firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
 		var user = firebase.auth().currentUser;
+		/*
 		user.sendEmailVerification().then(function() {
 		 // Email sent.
 		}, function(error) {
 			console.log('failed to send sendEmailVerification')
 		// An error happened.
 		});
+		*/
+		console.log("before fb")
 		firebase.database().ref('users/' + user.uid).update({
 			email:email,
 			name:name
+			}).then(function(){
+				var emailSplit = user.email.split(".");
+				var emailDash = emailSplit[0]+"-"+emailSplit[1];
+				firebase.database().ref('email/'+emailDash).set({
+					id:user.uid
+				});
+				window.location.href = "./home.html";
 			});
-			window.location.href = "./home.html";
+			
 		}
 
 		, function(error) {
@@ -90,6 +100,27 @@ function accountCreate() {
 	
 } ;
 
+function doForm() {
+	var workoutType = $('#workoutType').val();
+	var dateBlob = $('#myDate').val();
+	var dateList = dateBlob.split("-");
+	var day = dateList[2];
+	var month = dateList[1];
+	var year = dateList[0];
+	var doy = getDOY(day,month,year);
+	var duration = $('#duration').val();
+	var exertion = $('#exertion').val();
+	firebase.database().ref('activity/'+firebase.auth().currentUser.uid+"/"+year+"/"+doy).push({
+		workoutType:workoutType,
+		rawdate:dateBlob,
+		doy:doy,
+		duration:duration,
+		exertion:exertion
+	}).then(function() {window.location.href="./submitted.html"}) ;
+	
+
+}
+
 
 //Date Calculations
 function isLeapYear(year)  {
@@ -98,11 +129,124 @@ function isLeapYear(year)  {
 };
 
 // Get Day of Year
-function getDOY(day, month, year){
+function getDOY(dn, mn, year){
     var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-    var mn = this.getMonth();
-    var dn = this.getDate();
-    var dayOfYear = dayCount[mn] + dn;
-    if(mn > 1 && this.isLeapYear()) dayOfYear++;
+    var dayOfYear = dayCount[parseInt(mn)-1] + parseInt(dn);
+    if(mn > 1 && isLeapYear(year)) dayOfYear++;
     return dayOfYear;
 };
+
+function testEmailAsKey(){
+	var emailSplit = firebase.auth().currentUser.email.split(".");
+	var emailDash = emailSplit[0]+"-"+emailSplit[1];
+	firebase.database().ref('email/'+emailDash).set({
+		id:firebase.auth().currentUser.uid
+	}).then(function() {window.location.href="./lookup.html"}) ;
+}
+
+function lookupEmail() {
+	var email = $('#ewmaEmail').val();
+
+	var emailSplit = email.split(".");
+	var emailDash = emailSplit[0]+"-"+emailSplit[1];
+	
+
+	var endDate = $('#endDate').val();
+	var today = new Date();
+	var endDoy;
+	if(endDate==""){
+		
+		var day = today.getDate();
+		var month = today.getMonth()+1;
+		var year = today.getFullYear();
+		endDoy = getDOY(day, month, year);
+		console.log("no date provided")
+	} else {
+		var dateBlob = $('#endDate').val();
+		console.log("date provided: "+ dateBlob)
+		var dateList = dateBlob.split("-");
+		var day = dateList[2];
+		var month = dateList[1];
+		var year = dateList[0];
+		endDoy = getDOY(day,month,year);
+		var prevYear = false;
+		if(endDoy<28){
+			prevYear=true;
+			var lastYearsEnd = getDOY(31,12,year-1);
+			var firstDay = lastYearsEnd - 28 + endDoy;
+		}
+	}
+	console.log(endDoy)
+	var emailId = firebase.database().ref('/email/' + emailDash);
+	if (emailId) {
+		emailId.once("value", function(snapshot) {
+			if (snapshot.val()) {
+				var id = snapshot.val().id
+				console.log(id)
+				console.log("in value")
+				var activity = firebase.database().ref('/activity/' + id);
+				if (activity) {
+					activity.once("value", function(activityList) {
+						if (activityList.val()) {
+							var workoutsDict = activityList.val();
+							console.log("in got activity?")
+							console.log(workoutsDict)
+							var workouts = {};
+							var minIndex;
+							if(prevYear) {
+								console.log('prevYear')
+								console.log(workoutsDict[prevYear])
+								for(var i = firstDay; i<=lastYearsEnd; i++) {
+									if(typeof workoutsDict[year-1][i] != "undefined"){
+										if(!minIndex) {
+											minIndex = i - lastYearsEnd;
+										}
+										workouts[i-lastYearsEnd] = (workoutsDict[year-1][i])
+									}
+								}
+							}
+							console.log(workoutsDict[year])
+							for(var i = endDoy-28; i <=endDoy; i++) {
+								if(typeof workoutsDict[year][i] != "undefined"){
+									console.log(typeof workoutsDict[year][i])
+									if(!minIndex) {
+										minIndex = i;
+									}
+									workouts[i] = (workoutsDict[year][i])
+								}
+							}
+							console.log(workouts)
+							for (var i = minIndex; i <=endDoy; i++) {
+							    if (typeof workouts[i] != "undefined") {
+							    	console.log("defined workout")
+							    	var daysWorkouts = workouts[i];
+							    	var emwaSum28;
+							    	var emwaSum7;
+							    	for (var session in daysWorkouts) {
+							    		console.log("session here")
+							    		if (daysWorkouts.hasOwnProperty(session)) {
+							    			var sess = daysWorkouts[session];
+							    			console.log("should be here")
+							        		document.getElementById("workoutsTable").insertRow(-1).innerHTML =
+	"<tr><td>"+sess.rawdate+"</td><td>Duration: "+sess.duration+"</td> <td>Exertion: "+sess.exertion+"</td><td> type:"+sess.workoutType+"</td></tr>"
+							        	}
+							    	}
+
+							    }
+							}
+						}
+
+					});
+				} else {
+					console.log("no workouts found");
+				}
+			}
+		});
+	} else {
+		console.log('no email matching')
+	}
+
+
+
+
+}
